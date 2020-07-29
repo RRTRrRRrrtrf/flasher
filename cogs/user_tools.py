ba919b824c5a3cd3fa07ccfe2a1e2154e41fc69e
tuxlabore <tls.tuxla@gmail.com>
2020-07-29 07:40:26 +0000

    +f.mytag

--- cogs/user_tools.py
+++ cogs/user_tools.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import time
 import random
+from naomi_paginator import Paginator
 
 
 
@@ -26,6 +27,36 @@ class user_tools(commands.Cog):
 
 
 
+    @commands.command(aliases=['myDiscriminator'])
+    async def mytag(self,ctx):
+        """Ники пользователей который имеют такой же дискриминатор как и у вас
+        
+        Сменив ваш ник на один из таких ваш дискриминатор сменится на случайный
+        :warning: Discord устанавливает ограничения на смену ника и дискриминатора при нескольких использованиях"""
+        
+        names =[user.name
+                for user in self.bot.users
+                if user.name != ctx.author.name
+                and ctx.author.discriminator == user.discriminator]
+        random.shuffle(names)
+        
+        if len(names) > 100:
+            names = names[:100]
+        if len(names) == 0:
+            return await ctx.send(':no_entry: Нам не удалось найти кого либо с таким же дискриминатором как у вас')
+            
+        p = Paginator(ctx)
+        
+        for name in names:
+            p.add_page(
+                discord.Embed(
+                    title=name,
+                    color = random.randint(0x000000, 0xFFFFFF)
+                )
+                .set_footer(text='Смена вашего ника на какой либо из представленных повлечёт смену '
+                    f'вашего дискриминатора ({ctx.author.discriminator}) на случайный'))
+                
+        await p.call_controller()
 
 
 def setup(bot):
