2f7cdb487dfcd23263ab0c6beace28a3a407282a
tuxlabore <tls.tuxla@gmail.com>
2020-08-27 16:17:56 +0300

    Removed unused messages on error from error handler

--- utils/db.py
+++ utils/db.py
@@ -8,6 +8,7 @@ class SQL:
     def __init__(self, pool: asyncpg.pool.Pool):
         self.db = pool
 
+
     async def sql(self, code, *args):
         async with self.db.acquire() as connection:
             output = await connection.fetch(code, *args)
@@ -18,15 +19,20 @@ class SQL:
         else:
             return output
 
+
     async def rawGet(self, table: str, column: str, value):
         result = await self.sql(f'SELECT * FROM {table} WHERE {column}={value}')
         return result
 
+
+
 class PrefixesSQL(SQL):
     """Requests to DB associated with prefixes"""
+
     def __init__(self, pool: asyncpg.pool.Pool, config: dict):
         super().__init__(pool)
         self.standartValue = config.get('prefix')
+
 
     async def get(self, obj: Union[User, Guild]) -> str:
         """Get user/guild's prefix from DB"""
--- utils/error_handler.py
+++ utils/error_handler.py
@@ -10,140 +10,106 @@ class CommandErrorHandler(commands.Cog):
     """Error handler"""
     def __init__(self, bot):
         self.bot = bot
-        self.userToReport = None
-        self.chnToReport = None
-
 
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
-
-        if hasattr(ctx.command, 'on_error'):
-            return
         
         ignored = (commands.CommandNotFound)
-        ignored2 = (commands.BadArgument, commands.UserInputError)
         error = getattr(error, 'original', error)
         
         if isinstance(error, ignored):
             return
-        else:
-            if not isinstance(error, ignored2): 
-                await ctx.message.add_reaction('🚫')
 
-            if isinstance(error, commands.DisabledCommand):    
-                return
+        if isinstance(error, commands.DisabledCommand):
+            embed = discord.Embed(desctiption='Команда отключена',
+            color=discord.Colour.dark_red())    
+            return await ctx.send(embed)
 
-            elif isinstance(error, commands.NoPrivateMessage):
-                await ctx.send(embed=discord.Embed(timestamp=ctx.message.created_at, 
-                    description=':warning: Эта команда не может быть выполнена в личнных сообщениях.', 
-                    color=discord.Colour.dark_red()).set_footer(text=self.bot.user.name))
-                return
+        elif isinstance(error, commands.BadArgument):
+            ctx.command.reset_cooldown(ctx)
+            await ctx.invoke(self.bot.get_command("help"), command=str(ctx.command))
+            return
 
-            elif isinstance(error, commands.BadArgument):
-                ctx.command.reset_cooldown(ctx)
-                await ctx.invoke(self.bot.get_command("help"), command=str(ctx.command))
-                return
+        elif isinstance(error, commands.UserInputError):
+            ctx.command.reset_cooldown(ctx)
+            await ctx.invoke(self.bot.get_command("help"), command=str(ctx.command))
+            return
 
-            elif isinstance(error, commands.UserInputError):
-                ctx.command.reset_cooldown(ctx)
-                await ctx.invoke(self.bot.get_command("help"), command=str(ctx.command))
-                return
+        await ctx.message.add_reaction('🚫') # The reaction will be not added for errors upper
 
-            elif isinstance(error, commands.NotOwner):
+        if isinstance(error, commands.NotOwner):
 
-                await ctx.send(embed=discord.Embed(timestamp=ctx.message.created_at, 
-                    description=':warning: Эта команда может быть исполнена только владельцем бота.', 
-                    color=discord.Colour.dark_red()).set_footer(text=self.bot.user.name))
-                return
+            await ctx.send(embed=discord.Embed(timestamp=ctx.message.created_at, 
+                description=':warning: Эта команда может быть исполнена только владельцем бота.', 
+                color=discord.Colour.dark_red()).set_footer(text=self.bot.user.name))
+            return
 
-            elif isinstance(error, commands.errors.CommandOnCooldown):
-                retry_after = str(datetime.timedelta(seconds=int(error.retry_after)))
-                await ctx.send(embed=discord.Embed(timestamp=ctx.message.created_at,
-                    description=f':clock: Вы можете использовать эту комманду через {retry_after}',
-                    color=discord.Colour.dark_red()).set_footer(text=self.bot.user.name))
-                return
+        elif isinstance(error, commands.errors.CommandOnCooldown):
+            retry_after = str(datetime.timedelta(seconds=int(error.retry_after)))
+            await ctx.send(embed=discord.Embed(timestamp=ctx.message.created_at,
+                description=f':clock: Вы можете использовать эту комманду через {retry_after}',
+                color=discord.Colour.dark_red()).set_footer(text=self.bot.user.name))
+            return
 
-            elif isinstance(error, commands.errors.MissingPermissions):
-                i = 0
-                perms = []
-                for perm in error.missing_perms:
-                    i += 1
-                    p = f'{i}. {perm}'
-                    if i != len(error.missing_perms): p += ';'
-                    perms.append(p)
-                perms = '\n'.join(perms)
-                await ctx.send(embed=discord.Embed(timestamp=ctx.message.created_at, 
-                    title=f'Для исполнения этой комманды вы должны обладать следующими правами:', 
-                    description=f'```md\n{perms}\n```', 
-                    color=discord.Colour.dark_red()).set_footer(text=self.bot.user.name))
-                return
-            
-            elif isinstance(error, commands.MissingAnyRole):
-                i = 0
-                roles = []
-                for role in error.missing_roles:
-                    i += 1
-                    r = f'{i}. {role}'
-                    if i != len(error.missing_roles): r += ';'
-                    roles.append(r)
-                roles = '\n'.join(roles)
+        elif isinstance(error, commands.errors.MissingPermissions):
+            i = 0
+            perms = []
+            for perm in error.missing_perms:
+                i += 1 
+                p = f'{i}. {perm}'
+                if i != len(error.missing_perms): p += ';'
+                perms.append(p)
+            perms = '\n'.join(perms)
+            await ctx.send(embed=discord.Embed(timestamp=ctx.message.created_at, 
+                title=f'Для исполнения этой комманды вы должны обладать следующими правами:', 
+                description=f'```md\n{perms}\n```', 
+                color=discord.Colour.dark_red()).set_footer(text=self.bot.user.name))
+            return
 
-                await ctx.send(embed=discord.Embed(timestamp=ctx.message.created_at, 
-                    title=f'У вас нет ни одной из ниже указанных ролей для выполнения команды `{ctx.command}`:', 
-                    description=f'```md\n{roles}\n```',
-                    color=discord.Colour.dark_red()).set_footer(text=self.bot.user.name))
-                return
+        elif isinstance(error, commands.NSFWChannelRequired):
+            await ctx.send(embed=discord.Embed(timestamp=ctx.message.created_at, 
+                description=f'Эту комманду можно исполнять только в канале с пометкой NSFW', 
+                color=discord.Colour.red()).set_footer(text=self.bot.user.name))
+            return
 
-            elif isinstance(error, commands.MissingRole):
-                await ctx.send(embed=discord.Embed(timestamp=ctx.message.created_at,
-                    description=f'У вас нет роли `{error.missing_role}` для выполнения этой команды', 
-                    color=discord.Colour.dark_red()).set_footer(text=self.bot.user.name))
-                return
+        elif isinstance(error, discord.Forbidden):
+            perms = discord.Permissions(error.code)
+            needperms = [x for x in dir(perms) if not x.startswith('_') if getattr(perms, x) == True]
+            strperms = []
+            i = 0
+            for perm in needperms:
+                i += 1
+                r = f'{i}. {perm}'
+                if i != len(needperms): r += ';'
+                strperms.append(r)
+            strperms = '\n'.join(strperms)
 
-            elif isinstance(error, commands.NSFWChannelRequired):
-                await ctx.send(embed=discord.Embed(timestamp=ctx.message.created_at, 
-                    description=f'Эту комманду можно исполнять только в канале с пометкой NSFW', 
-                    color=discord.Colour.red()).set_footer(text=self.bot.user.name))
-                return
-
-            elif isinstance(error, discord.Forbidden):
-                perms = discord.Permissions(error.code)
-                needperms = [x for x in dir(perms) if not x.startswith('_') if getattr(perms, x) == True]
-                strperms = []
-                i = 0
-                for perm in needperms:
-                    i += 1
-                    r = f'{i}. {perm}'
-                    if i != len(needperms): r += ';'
-                    strperms.append(r)
-                strperms = '\n'.join(strperms)
-
-                await ctx.send(embed=discord.Embed(timestamp=ctx.message.created_at, 
-                    title=f'Боту должны быть предоставлены следующие права для выполнения этой команды:', 
-                    description=f'```md\n{strperms}\n```', color=discord.Colour.dark_red()).set_footer(text=self.bot.user.name))
-                return
+            await ctx.send(embed=discord.Embed(timestamp=ctx.message.created_at, 
+                title=f'Боту должны быть предоставлены следующие права для выполнения этой команды:', 
+                description=f'```md\n{strperms}\n```', color=discord.Colour.dark_red()).set_footer(text=self.bot.user.name))
+            return
 
 
-            elif isinstance(error, TooManyTries):
-                ctx.command.reset_cooldown(ctx)
-                await ctx.send(f'> Исполнение команды `{ctx.command.name}` вызваной {ctx.author.mention} было остановлено по причине слишком большого количества попыток ввести аргумент')
-                return
-            
-            elif isinstance(error, asyncio.TimeoutError):
-                ctx.command.reset_cooldown(ctx)
-                await ctx.send(f'> Исполнение команды `{ctx.command.name}` вызваной {ctx.author.mention} было остановлено по причине слишком большого использованого времени что бы ввести аргумент')
-                return
+        elif isinstance(error, TooManyTries):
+            ctx.command.reset_cooldown(ctx)
+            await ctx.send(f'> Исполнение команды `{ctx.command.name}` вызваной {ctx.author.mention} было остановлено по причине слишком большого количества попыток ввести аргумент')
+            return
+        
+        elif isinstance(error, asyncio.TimeoutError):
+            ctx.command.reset_cooldown(ctx)
+            await ctx.send(f'> Исполнение команды `{ctx.command.name}` вызваной {ctx.author.mention} было остановлено по причине слишком большого использованого времени что бы ввести аргумент')
+            return
 
-            elif isinstance(error, CanceledByUser):
-                ctx.command.reset_cooldown(ctx)
-                await ctx.send(f'> Исполнение команды `{ctx.command.name}` вызваной {ctx.author.mention} было остановлено автором сообщения')
-                return
+        elif isinstance(error, CanceledByUser):
+            ctx.command.reset_cooldown(ctx)
+            await ctx.send(f'> Исполнение команды `{ctx.command.name}` вызваной {ctx.author.mention} было остановлено автором сообщения')
+            return
 
-            elif isinstance(error, PrefixTooLong):
-                ctx.command.reset_cooldown(ctx)
-                await ctx.send(f'> Значение префикса не может быть больше чем 7 символов!')
-                return
+        elif isinstance(error, PrefixTooLong):
+            ctx.command.reset_cooldown(ctx)
+            await ctx.send(f'> Значение префикса не может быть больше чем 7 символов!')
+            return
         
         err = "\n".join(traceback.format_exception(type(error), error, error.__traceback__))
         chn = await self.bot.fetch_channel(self.bot.config['bugReportChannel'])
