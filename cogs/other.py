898f784748ee581e2369ff4e3853e32477482573
tuxlabore <tls.tuxla@gmail.com>
2020-09-22 00:25:06 +0300

    f.prefix; f.idea moved to other cog

--- cogs/other.py
+++ cogs/other.py
@@ -1,10 +1,11 @@
 import discord
 from discord.ext import commands, tasks
 
+import datetime
 from naomi_paginator import Paginator 
-from random import choice
+from random import choice, randint
 
-from utils.errors import PrefixTooLong  # pylint: disable=import-error
+from utils.errors import PrefixTooLong, TooManyTries, CanceledByUser # pylint: disable=import-error
 from utils.db import PrefixesSQL # pylint: disable=import-error
 
 class Other(commands.Cog):
@@ -110,6 +111,122 @@ class Other(commands.Cog):
             f'{self.bot.config.get("prefix")}help для просмотра списка команд')
         s = choice(status)
         await self.bot.change_presence(status=discord.Status.online, activity=discord.Game(s))
+
+    @commands.command(aliases=["suggestIdea", "bug", "idea"])
+    @commands.cooldown(1, 60, commands.BucketType.user)
+    async def suggest(self, ctx):
+        f"""Подайте идею для бота"""
+
+        def check(msg: discord.Message):
+            return msg.author.id == ctx.author.id
+
+        for i in range(5):
+            botMSG = await ctx.send(
+                "Введите тему идеи (не больше 60 символов).\n"
+                'Отправьте "Отмена" что бы отменить подачу идеи\n'
+                'или "Пропустить" что бы не подавать тему для идеи\n'
+            )
+            msg = await self.bot.wait_for("message", check=check, timeout=60)
+
+            topic = msg.content
+            try:
+                await msg.delete()
+            except:
+                pass
+
+            if topic.lower() in ("skip", "пропустить", "пропуск"):
+                topic = None
+                break
+            elif topic.lower() in ("отмена", "отменить", "cancel"):
+                raise CanceledByUser()
+
+            if len(topic) < 61:
+                await botMSG.delete()
+                break
+            if i == 4:
+                raise TooManyTries()
+
+        for i in range(5):
+            botMSG = await ctx.send(
+                "Введите описание идеи (не больше 512 символов).\n"
+                'Отправьте "Отмена" что бы отменить подачу идеи\n'
+            )
+            msg = await self.bot.wait_for("message", check=check, timeout=60)
+
+            description = msg.content
+            try:
+                await msg.delete()
+            except:
+                pass
+
+            if description.lower() in ("отмена", "отменить", "cancel"):
+                raise CanceledByUser()
+            if len(description) < 513:
+                await botMSG.delete()
+                break
+            if i == 4:
+                raise TooManyTries()
+
+        idea_number = len(await self.bot.sql("SELECT * FROM ideas;")) + 1
+        await self.bot.sql(
+            f"INSERT INTO ideas (topic,description,author,time) VALUES ($1,$2,$3,$4)",
+            topic,
+            description,
+            ctx.author.id,
+            int(datetime.datetime.now().timestamp()),
+        )
+
+        if not topic:
+            topic = "Тема не была установлена"
+
+        channel = await self.bot.fetch_channel(self.bot.config["ideaChannel"])
+
+        embed = discord.Embed(
+            title=f"Идея #{idea_number} от {ctx.author.name} • {topic}",
+            description=description,
+            timestamp=datetime.datetime.now(),
+        )
+        embed.set_author(
+            name=ctx.message.author.name, icon_url=str(ctx.author.avatar_url)
+        )
+        embed.set_footer(text="Идея была подана")
+
+        await channel.send(embed=embed)
+
+        embed = discord.Embed(
+            title=f"Ваша идея #{idea_number} отправлена успешно",
+            color=discord.Colour.green(),
+            url=self.bot.config["supportServerInvite"],
+        )
+        embed.add_field(name=topic, value=description)
+
+        await ctx.send(embed=embed)
+
+    @commands.command(aliases=["db"])
+    async def dashboard(self, ctx):
+        """Последние 15 записей с сервера поддержки."""
+        data = await self.bot.sql(
+            "SELECT * from dashboard ORDER BY time DESC LIMIT 15;"
+        )
+        p = Paginator(ctx)
+
+        for page in data:
+            embed = discord.Embed(
+                title=page["topic"],
+                description=page["content"],
+                timestamp=datetime.datetime.fromtimestamp(page["time"]),
+                color=randint(0x000000, 0xFFFFFF),
+            )
+            p.add_page(embed)
+
+        embed = discord.Embed(
+            title="Вы просмотрели последние записи",
+            description="Последние 15 записей просмотрены",
+            color=randint(0x000000, 0xFFFFFF),
+        )
+        p.add_page(embed)
+
+        await p.call_controller()
 
 def setup(bot):
     bot.add_cog(Other(bot))
